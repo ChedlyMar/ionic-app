8a28b94628327626d4e5209bfc577c4541529e7b
chedly <chedly.mar@gmail.com>
2020-06-18 10:21:08 +0100

    add recepes page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  recipes:{ id: string, title: string, imageUrl: string, ingredients:string[] } [] ;
+
+  constructor() {
+  this.recipes = [
+      {
+        id: 'r1',
+        title: 'Schnitzel',
+        imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Schnitzel.JPG/1024px-Schnitzel.JPG',
+        ingredients: ['French Fries', 'Pork Meat', 'Salad']
+      },
+      {
+        id: 'r2',
+        title: 'Spaghetti',
+        imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/1024px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+        ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+      }
+    ];
+  }
 
 }
